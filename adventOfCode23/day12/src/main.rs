98432a5718858c2da2f1e7a6966ed7afe8bbe395
Vetle Roos Mangrud <vmangrud@gmail.com>
2024-12-11 12:05:11 +0100

    Yesyesyesyes

--- adventOfCode23/day12/src/main.rs
+++ adventOfCode23/day12/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::{collections::{HashMap, HashSet}, fs, hash::Hash, str::Chars};
 
 use itertools::Itertools;
 
@@ -42,68 +42,40 @@ fn part1(input:String){
     dbg!(total);
 }
 fn part2(input:String){
-    let mut total: u64 = 0;
-    for (i,line) in input.lines().enumerate() {
+    // Struggled a lot with part 2. Watched HyperNeutrinos solve of this part, and my solution is therefore very similar
+    let mut total = 0;
+    for line in input.lines() {
         let chars:Vec<_> = (line.split(" ").next().unwrap().to_owned()+"?").repeat(5).strip_suffix("?").unwrap().chars().collect();
         let groups: Vec<usize> = (line.split(" ").nth(1).unwrap().to_owned()+",").repeat(5).strip_suffix(",").unwrap().split(",").map(|n| n.parse().unwrap()).collect();
-        let mut possibles:HashSet<String> = HashSet::from([String::from("")]);
-        for char in chars {
-            let mut next_possibles: HashSet<String> = HashSet::new();
-            for possible in &possibles {
-                let mut new_strings = Vec::new();
-                if char == '.' || char == '?' {
-                    new_strings.push(possible.clone() + ".");
-                } 
-                if char == '#' || char == '?' {
-                    new_strings.push(possible.clone() + "#");
-                }
-                new_strings.iter().cloned().filter(|p| verify_possible(p, &groups)).for_each(|p| {next_possibles.insert(p);});
-            }
-            possibles = next_possibles;
-        }
-        let final_possibles: Vec<_> = possibles.iter().cloned().filter(|p| verify_complete(p, &groups)).collect();
-        total += final_possibles.len() as u64;
-        dbg!(i);
+        let mut map = HashMap::new();
+        let subtotal = get_possible_count(&chars, 0, &groups,0, &mut map);
+        total += subtotal;
     }
     dbg!(total);
 }
-fn verify_complete(start:&String, groups:&Vec<usize>) -> bool {
-    let mut in_group = false;
-    let mut found_groups = Vec::new();
-    for c in start.chars() {
-        if c == '#' {
-            if in_group {
-                *found_groups.last_mut().unwrap() += 1;
-            } else {
-                in_group = true;
-                found_groups.push(1);
-            }
-        } else {
-            in_group = false;
-        }
+fn get_possible_count(chars: &Vec<char>, chars_i:usize, groups: &Vec<usize>, groups_i:usize, map: &mut HashMap<(usize,usize),i64>) -> i64 {
+    if groups.len() <= groups_i{
+        if chars.iter().copied().skip(chars_i).all(|c| c != '#') {return 1;} else {return 0;}
     }
-    found_groups == *groups
-}
-fn verify_possible(start:&String, groups:&Vec<usize>) -> bool {
-    if start.is_empty() {return true;}
-    let mut in_group = false;
-    let mut found_groups = Vec::new();
-    for c in start.chars() {
-        if c == '#' {
-            if in_group {
-                *found_groups.last_mut().unwrap() += 1;
-            } else {
-                in_group = true;
-                found_groups.push(1);
-            }
-        } else {
-            in_group = false;
-        }
+    else if chars.len() <= chars_i {return 0;}
+    match map.get(&(chars_i, groups_i)) {
+       Some(v) => return *v,
+       None => (), 
     }
-    if found_groups.is_empty() {return true;}
-    for i in 0..found_groups.len()-1 {
-        if !found_groups.get(i).unwrap() == *groups.get(i).unwrap() {return false;}
-    }
-    in_group && found_groups.last() <= groups.get(found_groups.len()-1) || found_groups.last() == groups.get(found_groups.len()-1)
-}
 
+    let mut total = 0;
+    if chars[chars_i] == '.' || chars[chars_i] == '?' {
+        total += get_possible_count(chars, chars_i+1, groups, groups_i, map);
+    }
+    if chars[chars_i] == '#' || chars[chars_i] == '?' {
+        let next_chars = chars.iter().skip(chars_i).copied();
+        let enough_left = chars.len() - chars_i >= groups[groups_i];
+        let next_n_broken = next_chars.clone().take(groups[groups_i]).all(|c| c == '#' || c == '?');
+        let fixed_after = next_chars.skip(groups[groups_i]).take(1).all(|c| c== '.' || c== '?') ;
+        if enough_left && next_n_broken && fixed_after{
+            total += get_possible_count(chars, chars_i + groups[groups_i]+1, groups, groups_i + 1, map);
+        }
+    }
+    map.insert((chars_i,groups_i), total);
+    total
+}
